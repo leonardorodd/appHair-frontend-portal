167ee1f45c5eb4d1b8baf7ac4ee60d3962d36cac
leonrodd <leonardorodd128@gmail.com>
2021-01-30 21:59:14 -0300

    Initial commit

--- server.js
+++ server.js
@@ -3,9 +3,7 @@ const PORT = process.env.PORT || 3000;
 const path = require('path');
 const app = express();
 
-/* app.use(express.static('dist'));
- */
-app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static('build'));
 
 app.listen(PORT, ()=> console.log(`Listening on port ${PORT}`));
 
